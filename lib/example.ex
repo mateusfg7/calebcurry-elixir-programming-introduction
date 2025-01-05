2ab5650768740e015536a4b263f5e212da32a218
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2025-01-05 13:05:20 -0300

    Working with enumarators and tuples

--- lib/example.ex
+++ lib/example.ex
@@ -1,12 +1,33 @@
 defmodule Example do
   use Application
+  alias UUID
 
   def start(_type, _args) do
-    IO.puts(Example.hello())
+    Example.main()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  def hello do
-    :hello
+  def main do
+    memberships = {:bronze, :silver}
+    memberships = Tuple.insert_at(memberships, tuple_size(memberships), :gold)
+    IO.inspect(memberships)
+
+    prices = {5, 10, 15}
+    avg = Tuple.sum(prices) / tuple_size(prices)
+    IO.puts(avg)
+
+    IO.puts(
+      "Average price from #{elem(memberships, 0)} #{elem(memberships, 1)} #{elem(memberships, 2)} is #{avg}"
+    )
+
+    users = [
+      {"Mateus", :gold},
+      {"Felipe", :gold},
+      {"Maria", :silver}
+    ]
+
+    Enum.each(users, fn {name, membership} ->
+      IO.puts("#{name} has a #{membership} membership.")
+    end)
   end
 end
