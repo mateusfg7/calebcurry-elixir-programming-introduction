a93361a11dc1238e609795785fe362f655210246
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2025-01-12 19:49:25 -0300

    Guessing game

--- lib/example.ex
+++ lib/example.ex
@@ -1,11 +1,3 @@
-defmodule Membership do
-  defstruct [:type, :price]
-end
-
-defmodule User do
-  defstruct [:name, :membership]
-end
-
 defmodule Example do
   use Application
   alias UUID
@@ -16,20 +8,17 @@ defmodule Example do
   end
 
   def main do
-    gold_membership = %Membership{type: :gold, price: 25}
-    silver_membership = %Membership{type: :silver, price: 20}
-    bronze_membership = %Membership{type: :bronze, price: 15}
-    _none_membership = %Membership{type: :none, price: 0}
+    correct = :rand.uniform(11) - 1
 
-    users = [
-      %User{name: "Mateus", membership: gold_membership},
-      %User{name: "Felipe", membership: gold_membership},
-      %User{name: "Maria", membership: silver_membership},
-      %User{name: "Clara", membership: bronze_membership}
-    ]
+    guess =
+      IO.gets("Guess a number between 0 and 10 (#{correct}): ")
+      |> String.trim()
+      |> String.to_integer()
 
-    Enum.each(users, fn %User{name: name, membership: membership} ->
-      IO.puts("#{name} has a #{membership.type} membership paying #{membership.price}.")
-    end)
+    if guess == correct do
+      IO.puts("You win!")
+    else
+      IO.puts("You loose!")
+    end
   end
 end
