b0e5e01a5b7dd81b2399b6539fef5524b6b7b329
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2025-01-12 20:02:47 -0300

    Interger parses and pattern matching with `case`

--- lib/example.ex
+++ lib/example.ex
@@ -1,6 +1,5 @@
 defmodule Example do
   use Application
-  alias UUID
 
   def start(_type, _args) do
     Example.main()
@@ -9,16 +8,27 @@ defmodule Example do
 
   def main do
     correct = :rand.uniform(11) - 1
+    IO.puts("Correct is #{correct}")
 
     guess =
-      IO.gets("Guess a number between 0 and 10 (#{correct}): ")
+      IO.gets("Guess a number between 0 and 10: ")
       |> String.trim()
-      |> String.to_integer()
+      |> Integer.parse()
 
-    if guess == correct do
-      IO.puts("You win!")
-    else
-      IO.puts("You loose!")
+    IO.inspect(guess)
+
+    case guess do
+      {result, _} ->
+        IO.puts("Parse successful #{result}")
+
+        if result == correct do
+          IO.puts("You win!")
+        else
+          IO.puts("You loose")
+        end
+
+      :error ->
+        IO.puts("Something went wrong")
     end
   end
 end
